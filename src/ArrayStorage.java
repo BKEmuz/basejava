244d706192c0de96b2c4f37d4c8e08fa5aa68da5
Konstantin Bakhturov <bakhturov@gmail.com>
2020-03-14 22:48:19 +0300

    Mistakes corrected v.1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -11,11 +12,14 @@ public class ArrayStorage {
         }
     }
 
+    int size = 0;
+
     void save(Resume r) {
         int i = 0;
         while (i < storage.length) {
             if (storage[i] == null) {
                 storage[i] = r;
+                this.size = ++i;
                 break;
             }
             i++;
@@ -24,10 +28,8 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         int i = 0;
-        while (i < storage.length) {
-           if (storage[0] == null) {
-               break;
-           } else if (storage[i].uuid == uuid) {
+        while (i < size) {
+            if (storage[i].uuid == uuid) {
                 return storage[i];
             }
             i++;
@@ -36,39 +38,28 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int nElems = storage.length;                    //количество элементов
-        int j;                                          //счетчик цикла
-        String searchKey;                               //ключи искомого элемента
-        searchKey = uuid;                               //удаление элемента с ключом  uuid
-
-        for (j = 0; j < nElems; j++) {                  //поиск удаляемого элемента
-            if (storage[j].uuid == searchKey)
+        int j;
+        for (j = 0; j < storage.length; j++) {                  //поиск удаляемого элемента
+            if (storage[j].uuid == uuid)
                 break;
         }
 
-        for (int k = j; k < nElems - 1; k++) {           //сдвиг последующих элементов
+        for (int k = j; k < storage.length - 1; k++) {           //сдвиг последующих элементов
             storage[k] = storage[k + 1];
-            nElems--;
         }
+        size--;
     }
-        /**
-         * @return array, contains only Resumes in storage (without null)
-         */
 
-        private int nL;
-        Resume[] getAll () {
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
 
-            for (int i = 0; i < storage.length; i++) {
-                if (storage[i] == null) {
-                    nL = i;
-                    break;
-                }
-            }
-            return Arrays.copyOfRange(storage, 0, nL);
-        }
+        return Arrays.copyOf(storage, size);
+    }
 
-        int size () {
-            return getAll().length;
-        }
+    int size() {
+        return size;
+    }
 }
 
