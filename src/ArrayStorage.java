4a640306ccd85193236b363aac7dad6a00bd2612
Konstantin Bakhturov <bakhturov@gmail.com>
2020-03-14 13:24:24 +0300

    Class ArrayStorage update using cycles and massivies only

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +6,69 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int i = 0;
+        while (i < storage.length) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+            i++;
+        }
     }
 
     Resume get(String uuid) {
+        int i = 0;
+        while (i < storage.length) {
+           if (storage[0] == null) {
+               break;
+           } else if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+            i++;
+        }
         return null;
     }
 
     void delete(String uuid) {
-    }
+        int nElems = storage.length;                    //количество элементов
+        int j;                                          //счетчик цикла
+        String searchKey;                               //ключи искомого элемента
+        searchKey = uuid;                               //удаление элемента с ключом  uuid
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+        for (j = 0; j < nElems; j++) {                  //поиск удаляемого элемента
+            if (storage[j].uuid == searchKey)
+                break;
+        }
 
-    int size() {
-        return 0;
+        for (int k = j; k < nElems - 1; k++) {           //сдвиг последующих элементов
+            storage[k] = storage[k + 1];
+            nElems--;
+        }
     }
+        /**
+         * @return array, contains only Resumes in storage (without null)
+         */
+
+        private int nL;
+        Resume[] getAll () {
+
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] == null) {
+                    nL = i;
+                    break;
+                }
+            }
+            return Arrays.copyOfRange(storage, 0, nL);
+        }
+
+        int size () {
+            return getAll().length;
+        }
 }
+
